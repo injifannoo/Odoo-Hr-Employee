3eaef0e941049ffc8bcc698575a3d4b64fa760f5
injifannoo <injifanotu@gmail.com>
2025-08-09 20:46:39 +0300

    Department  model is added

--- hr_system/models/department.py
+++ hr_system/models/department.py
@@ -1,9 +1,9 @@
 from odoo import models, fields
 
 class Department(models.Model):
-    _name = 'hr.department'
+    _name = 'hr_system.department'
     _description = 'Department'
 
     name = fields.Char(string="Department Name", required=True)
-    manager_id = fields.Many2one('hr.employee', string="Manager")
-    employee_ids = fields.One2many('hr.employee', 'department_id', string="Employees")
+    manager_id = fields.Many2one('hr_system.employee', string="Manager")
+    employee_ids = fields.One2many('hr_system.employee', 'department_id', string="Employees")
