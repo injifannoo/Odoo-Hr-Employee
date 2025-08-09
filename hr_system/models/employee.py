4c6e4fa55115f52f76383c1b48e6e18936eb6ccb
injifannoo <injifanotu@gmail.com>
2025-08-09 20:48:44 +0300

    Employee model and view is modified

--- hr_system/models/employee.py
+++ hr_system/models/employee.py
@@ -1,10 +1,16 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HREmployee(models.Model):
-    _name = 'hr.employee'
+    _name = 'hr_system.employee'
     _description = 'Employee Record'
 
     name = fields.Char(string='Name', required=True)
     age = fields.Integer(string='Age')
-    department = fields.Char(string='Department')
-    department_id = fields.Many2one('hr.department', string="Department")
+    department_id = fields.Many2one('hr_system.department', string="Department")
+    retirement_age = fields.Integer(string='Retirement Age', compute='_compute_retirement_age')
+
+    @api.depends('age')
+    def _compute_retirement_age(self):
+        for rec in self:
+            rec.retirement_age = 65 - rec.age if rec.age else 0
+
